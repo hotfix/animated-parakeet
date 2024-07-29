60948631d358e3572cdae93352867bc0966dd177
Alexander <alexander.albrant@gmail.com>
2024-07-29 19:19:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Alexandrie Albrant",
+  title: "Software Developer & Husband & Dad",
+  email: "alex@shureg.de",
+  gitHub: "hotfix",
+  instagram: "habekeinnick",
+  linkedIn: "alexanderalbrant",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "habekeinnick",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
